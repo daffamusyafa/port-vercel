c594e3642f5c8912922f1d7ba9c5f4724b4f5782
daffamusyafa <121594058+daffamusyafa@users.noreply.github.com>
2025-11-04 20:48:09 +0700

    Fix import case for Sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/sidebar";
 import { DarkModeProvider } from "@/components/DarkMode";
 
 const geistSans = Geist({
